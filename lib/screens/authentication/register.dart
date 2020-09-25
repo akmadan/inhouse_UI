ed1f452001c57334e2118d1cdabaf804856b2419
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2020-09-25 18:21:13 +0530

    first

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -148,61 +148,7 @@ class _RegisterState extends State<Register> {
                 ),
               ),
             ),
-            // child: Form(
-            //   key: _formKey,
-            //   child: Column(
-            //     children: <Widget>[
-            //       SizedBox(height: 20.0),
-            //       TextFormField(
-            //           decoration:
-            //               textInputDecoration.copyWith(hintText: 'Email'),
-            //           validator: (val) =>
-            //               val.isEmpty ? 'Enter an email' : null,
-            //           onChanged: (val) {
-            //             setState(() => email = val);
-            //           }),
-            //       SizedBox(height: 20.0),
-            //       TextFormField(
-            //           decoration: textInputDecoration.copyWith(
-            //               hintText: 'Password'),
-            //           obscureText: true,
-            //           validator: (val) => val.length < 6
-            //               ? 'Enter a password 6+ chars long'
-            //               : null,
-            //           onChanged: (val) {
-            //             setState(() => password = val);
-            //           }),
-            //       SizedBox(height: 20.0),
-            //       RaisedButton(
-            //         color: Colors.blue[300],
-            //         child: Text(
-            //           'Register',
-            //           style: TextStyle(
-            //             color: Colors.white,
-            //           ),
-            //         ),
-            // onPressed: () async {
-            //   if (_formKey.currentState.validate()) {
-            //     setState(() => loading = true);
-            //     dynamic result = await _auth
-            //         .registerWithEmailAndPassword(email, password);
-            //     if (result == null) {
-            //       setState(() {
-            //         error = 'Invalid credentials';
-            //         loading = false;
-            //       });
-            //     }
-            //   }
-            // },
-            //       ),
-            //       SizedBox(height: 12.0),
-            //       Text(
-            //         error,
-            //         style: TextStyle(fontSize: 14.0),
-            //       ),
-            //     ],
-            //   ),
-            // )
+           
           );
   }
 }
